f53287cd3a9915eef00241633bb3cbbdddf3749d
thwolter <thwolter@gmail.com>
2017-04-07 08:30:02 +0200

    working on portfolio.blade

--- resources/views/portfolios/show.blade.php
+++ resources/views/portfolios/show.blade.php
@@ -2,93 +2,48 @@
 
 @section('content')
 
-    <div>
-        <!-- header-portfolio -->
-        <div class="header-portfolio">
-            <div class="pull-left">
-                <span class="title">Mein Europa Portfolio</span>
-                <a href="#"><icon class="glyphicon glyphicon-pencil">Bearbeiten</a>
-            </div>
-            <div class="pull-right">
-                <span class="key">Aktuell</span>
+    <section class="portfolio-container">
+        <header class="portfolio-header">
+            <h2 class="title">Mein Europa Portfolio</h2>
+            <a href="#"><icon class="glyphicon glyphicon-pencil"></icon>Bearbeiten</a>
+
+            <div class="value-box">
+                <label class="label">Aktuell</label>
                 <span class="value">1.200,33</span>
             </div>
-            <div class="pull-right">
-                <span class="key">Risiko</span>
+
+            <div class="value-box">
+                <label class="label">Risiko</label>
                 <span class="value">234,43</span>
             </div>
-            <div class="clearfix"></div>
-        </div>
-        <hr>
+        </header>
 
-        <div class="portfolio-wrapper">
-            <!-- nav-portfolio -->
-            <div class="navbar-portfolio">
-                <ul class="nav navbar-nav">
+        <div class="portfolio-content">
+            <!-- sidebar navigation -->
+            <nav class="nav-portfolio">
+                <ul>
                     <li><a href="#">Überblick</a></li>
                     <li><a href="#">Transaktionen</a></li>
                     <li><a href="#">Marktwerte</a></li>
                     <li><a href="#">Risiko</a></li>
                     <li><a href="#">Optimieren</a></li>
                 </ul>
+            </nav>
+
+            <!--content-portfolio -->
+            <div class="content-cell">
+                <p> here goes content</p>
             </div>
-        </div>
-
-
-        <!--content-portfolio -->
-        <div class="content-portfolio">
-            <p> here goes content</p>
-        </div>
-
-        <div class="clearfix"></div>
-        <btn class="btn btn-primary pull-right">Neue Transaktion</btn>
-
-
-
-    </div>
-
-
-
-
-
-
-
-    <!-- show single portfolio -->
-    <div class="panel panel-default">
-        <div class="panel-heading">Portfolios</div>
-
-        <div class="panel-body">
-
-            <article>
-                <h4>
-                    {{ $portfolio->name }}
-                </h4>
-                <div class="body">
-                    {{ $portfolio->currency }}
-                </div>
-
-
-                <div class="input-group-btn">
-
-                    <a
-
-                    <a class="btn btn-primary inline-block-tight pull-right"
-                       href="/portfolios/{{ $portfolio->id }}/edit">Edit</a>
-
-
-                    <form method="post" action="/portfolios/{{ $portfolio->id }}">
-
-                        {{ csrf_field() }}
-
-                        <input type="hidden" name="_method" value="DELETE">
-                        <button type="submit" class="btn btn-default inline-block-tight pull-right">Löschen</button>
-                    </form>
-
-                </div>
-            </article>
 
         </div>
-    </div>
+    </section>
+
+
+
+
+
+
+
 @endsection
 
 
