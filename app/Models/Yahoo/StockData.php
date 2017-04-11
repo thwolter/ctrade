67346dcfeeb7b33a7a82d5d6b72bbf8290a5eced
Thomas Wolter <thwolter@gmail.com>
2017-04-11 12:36:17 +0000

    experimental

---
+++ app/Models/Yahoo/StockData.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Thomas
+ * Date: 08.04.17
+ * Time: 19:35
+ */
+
+namespace App\Library;
+
+
+class StockData extends MarketData
+{
+
+    /**
+     * Dynamically retrieve attributes on the model
+     *
+     * @param string $key
+     * @return mixed
+     */
+    public function __get($key) {
+
+        $data = $this->getData('getQuotes')['query']['results']['quote'];
+
+        if (array_key_exists($key, $data)) return $data[$key];
+    }
+}
