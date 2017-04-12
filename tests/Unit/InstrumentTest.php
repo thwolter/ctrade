03f12b7ce0b4e7ffddff5b33d48ff7549135151a
thwolter <thwolter@gmail.com>
2017-04-12 14:15:51 +0200

    implement Yahoo search

---
+++ tests/Unit/InstrumentTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class InstrumentTest extends TestCase
+{
+
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+}
