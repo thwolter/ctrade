f00a53fd85c43fd5eedc8ec49c5e2cf346cdf7d5
Thomas Wolter <thwolter@gmail.com>
2017-05-31 15:03:21 +0000

    adjust portfolio blades

--- resources/views/layouts/portfolio.blade.php
+++ resources/views/layouts/portfolio.blade.php
@@ -2,32 +2,45 @@
 
 @section('content')
 
-    <div class="row">
-        <div class="col-md-3">
-            <!-- sidebar navigation -->
-            @include('layouts.sidebar')
+    <section id="content-region-3" class="padding-40 page-tree-bg">
+        <div class="container">
+            <h3 class="page-tree-text">
+                Meine Portfolios
+            </h3>
         </div>
+    </section><!--page-tree end here-->
+    <div class="space-70"></div>
+    
+    <div class="container">
+        <div class="row">
 
-        <div class="col-md-9">
-            <div class="panel panel-primary">
+            <div class="col-md-3">
+                <!-- sidebar navigation -->
+                @include('layouts.sidebar')
+            </div>
 
-                <div class="panel-heading">
-                    <h3 class="panel-title">
-                        {{ $portfolio->name }}
-                        <span class="pull-right">
-                            <a href="{{ route('portfolios.edit', $portfolio->id) }}">Einstellungen</a>
-                        </span>
-                    </h3>
-                </div>
-
-                <div class="panel-body">
-                    @include('partials.pills')
-                    @yield('container-content')
-                </div>
-
-                <!-- footer -->
-                <div class="">
-
+            <div class="col-md-9">
+                <div class="panel panel-primary">
+    
+                    <div class="panel-heading">
+                        <h3 class="panel-title">
+                            {{ $portfolio->name }}
+                            <span class="pull-right">
+                                <a href="{{ route('portfolios.edit', $portfolio->id) }}">Einstellungen</a>
+                            </span>
+                        </h3>
+                    </div>
+    
+           
+                    <div class="panel-body">
+                        @include('partials.pills')
+                        @yield('container-content')
+                    </div>
+    
+                    <!-- footer -->
+                    <div class="">
+    
+                    </div>
                 </div>
             </div>
         </div>
--- resources/views/portfolios/create.blade.php
+++ resources/views/portfolios/create.blade.php
@@ -2,47 +2,57 @@
 
 @section('content')
 
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Portfolio erstellen</div>
-                <div class="panel-body">
-                    {!! Form::open(['route' => 'portfolios.store', 'class' => "form-horizontal"]) !!}
+    <section id="content-region-3" class="padding-40 page-tree-bg">
+        <div class="container">
+            <h3 class="page-tree-text">
+                Portfolio anlegen
+            </h3>
+        </div>
+    </section><!--page-tree end here-->
+    <div class="space-70"></div>
+    
 
-                    @include('partials.errors')
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6 offset-lg-3">
+                <div class="panel panel-default">
+                    <div class="my-login-form">
+                
+                        {!! Form::open(['route' => 'portfolios.store']) !!}
+                        <h4 class="text-uppercase">Portfolio erstellen</h4><hr>
+                    
+                        @include('partials.errors')
 
-                    <div class="form-group">
-                        {!! Form::label('name', 'Name', ['class' => 'col-md-4 control-label']) !!}
-                        <div class="col-md-6">
-                            {!! Form::text('name', null, ['class' => 'form-control']) !!}
-                            <span class="help-block">Wie soll dein Portfolio heißen?</span>
-                        </div>
-                    </div>
+                        <!-- portfolio name -->
+                        <div class="form-group row">
+                            {!! Form::label('name', 'Name', ['class' => 'col-md-3 col-form-label']) !!}
+                            <div class="col-md-8">
+                                {!! Form::text('name', null, ['class' => 'form-control']) !!}
+                            </div>
+                        </div><!-- /portfolio name -->
 
-                    <div class="form-group">
-                        {!! Form::label('currency', 'Währung', ['class' => 'col-md-3 col-md-offset-1 control-label']) !!}
-                        <div class="col-md-6">
-                            {!! Form::select('currency', $currencies, null, ['class' => 'form-control']) !!}
-                            <span class="help-block">
-                                Währung, in der das Portfolio geführt wird.
-                            </span>
-                        </div>
-                    </div>
+                        <!-- currency -->
+                        <div class="form-group row">
+                            {!! Form::label('currency', 'Währung', ['class' => 'col-md-3 col-form-label']) !!}
+                            <div class="col-md-8">
+                                {!! Form::select('currency', $currencies, null, ['class' => 'form-control']) !!}
+                            </div>
+                        </div><!-- /currecny -->
 
-                    <div class="form-group">
-                        {!! Form::label('cash', 'Barbestand', ['class' => 'col-md-3 col-md-offset-1 control-label']) !!}
-                        <div class="col-md-6">
-                            {!! Form::text('cash', null, ['class' => 'form-control col-md-3']) !!}
-                            <span class="help-block">
-                                Barbestand in Portfoliowährung.
-                            </span>
-                        </div>
-                    </div>
+                        <!-- cash -->
+                        <div class="form-group row">
+                            {!! Form::label('cash', 'Barbestand', ['class' => 'col-md-3 col-form-label']) !!}
+                            <div class="col-md-8">
+                                {!! Form::text('cash', null, ['class' => 'form-control']) !!}
+                                <span class="help-block">
+                                    Barbestand in Portfoliowährung.
+                                </span>
+                            </div>
+                        </div><!-- /cash -->
 
-                    <div class="form-group">
-                        <div class="col-md-6 col-md-offset-4">
-                            {!! Form::submit('Portfolio erstellen', ['class' => 'btn btn-primary']) !!}
-                            <a href="{{ URL::previous() }}" class="btn btn-default">Abbrechen</a>
+                    <div class="text-right">
+                            {!! Form::submit('Erstellen', ['class' => 'btn theme-btn-color']) !!}
+                            <button href="{{ URL::previous() }}" class="btn theme-btn-outline">Abbrechen</button>
                         </div>
                     </div>
 
