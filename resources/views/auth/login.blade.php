62c25755dfca41fea764ee674b6020b8111529b3
thwolter <thwolter@gmail.com>
2017-05-31 07:29:55 +0200

    new format for login and master

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,19 +1,29 @@
 @extends('layouts.master')
 
 @section('content')
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2 ">
-            <div class="panel panel-default">
-                <div class="panel-heading">Login</div>
-                <div class="panel-body">
-                    <form class="form-horizontal" role="form" method="POST" action="{{ route('login') }}">
+
+    <section id="content-region-3" class="padding-40 page-tree-bg">
+        <div class="container">
+            <h3 class="page-tree-text">
+                Login into your account
+            </h3>
+        </div>
+    </section><!--page-tree end here-->
+    <div class="space-70"></div>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6 offset-lg-3">
+                <div class="panel panel-default">
+                    <div class="my-login-form">
+
+                        <form role="form" method="POST" action="{{ route('login') }}">
                         {{ csrf_field() }}
 
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Addresse</label>
+                            <h4 class="text-uppercase">Login</h4><hr>
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
+                            <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                                <input id="email" type="email" class="form-control" name="email" placeholder="Email.." value="{{ old('email') }} " required autofocus>
 
                                 @if ($errors->has('email'))
                                     <span class="help-block">
@@ -21,13 +31,10 @@
                                     </span>
                                 @endif
                             </div>
-                        </div>
 
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Passwort</label>
+                            <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
 
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password" required>
+                                <input id="password" type="password" class="form-control" name="password" placeholder="Password.." required>
 
                                 @if ($errors->has('password'))
                                     <span class="help-block">
@@ -35,39 +42,28 @@
                                     </span>
                                 @endif
                             </div>
-                        </div>
 
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
+                            <div class="form-group">
                                 <div class="checkbox">
                                     <label>
                                         <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Eingeloggt bleiben
                                     </label>
                                 </div>
                             </div>
-                        </div>
 
-                        <div class="form-group">
-                            <div class="col-md-8 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Login
-                                </button>
-
-                                <a class="btn btn-link" href="{{ route('password.request') }}">
-                                    Passwort vergessen?
-                                </a>
+                            <div class="text-right">
+                                <button type="submit" class="btn theme-btn-color">Login</button>
                             </div>
-                        </div>
+                            <hr>
 
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <a class="btn btn-link" href="{{ route('register') }}">
-                                    Noch keinen Account?
-                                </a>
+                            <div class="form-btm-link text-center">
+                                <a class="btn btn-link" href="{{ route('password.request') }}">Passwort vergessen?</a>
+                                <span>oder</span>
+                                <a class="btn btn-link" href="{{ route('register') }}">Account erstellen</a>
                             </div>
-                        </div>
 
-                    </form>
+                        </form><!--login form end-->
+                    </div>
                 </div>
             </div>
         </div>
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -3,44 +3,39 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
-    <!-- Styles -->
-    <link href="{{ asset('css/styles.css') }}" rel="stylesheet">
+    <!--plugins-->
+    <link href="{{ asset('css/plugins/plugins.css') }}" rel="stylesheet">
+
+    <!--Custom css-->
+    <link href="{{ asset('css/style.css') }}" rel="stylesheet">
 
     <!-- Scripts -->
     <script>window.Laravel = {!! json_encode(['csrfToken' => csrf_token(), ]) !!}; </script>
+
+    <!-- jQuery plugins. -->
+    <script src="{{ asset('js/plugins/plugins.js') }}"></script>
+    <script src="{{ asset('js/app.js') }}"></script>
 </head>
 
 <body>
-    <div id="app" class="container">
 
-        <!-- app-bar with login -->
-        <div class="hidden-xs app-bar">
+    @include('layouts.appbar')
 
-            <!-- Branding Image -->
-            <a class="navbar-brand" href="{{ url('/') }}">
-                {{ config('app.name', 'Laravel') }}
-            </a>
+    @yield('content')
 
-            @include('layouts.appbar')
-        </div>
+    <!--back to top-->
+    <a href="#" class="scrollToTop"><i class="ion-android-arrow-dropup-circle"></i></a>
+    <!--back to top end-->
 
-        <div>
+    @include('layouts.footer')
 
-            @yield('content')
-
-        </div>
-
-    </div>
-
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}"></script>
 
 </body>
 </html>
