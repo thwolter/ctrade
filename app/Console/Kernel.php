b6956a3d4259941da5232236fc0fafc7a359e02b
thwolter <thwolter@gmail.com>
2017-06-10 21:07:15 +0200

    store settings in respective model, differentiate commands by provider and database for scheduling

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use App\Console\Commands\CacheQuandlSSE;
 use App\Console\Commands\UpdateQuandlSSE;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
@@ -28,7 +29,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command(UpdateQuandlSSE::class)->dailyAt('23:00');
-        $schedule->command(UpdateQuandlSSE::class)->dailyAt('23:00');
+        $schedule->command(CacheQuandlSSE::class)->dailyAt('00:00');
         // $schedule->command('inspire')
         //          ->hourly();
     }
