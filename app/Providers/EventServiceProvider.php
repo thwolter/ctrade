a1689dc66aea59df9c953f3dd146e75bd9fe609d
thwolter <thwolter@gmail.com>
2017-06-14 08:26:02 +0200

    create metadata event and listener

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -13,8 +13,8 @@ class EventServiceProvider extends ServiceProvider
      * @var array
      */
     protected $listen = [
-        'App\Events\DatasourceCreated' => [
-            'App\Listeners\LogDatasourceCreated',
+        'App\Events\MetadataHasUpdated' => [
+            'App\Listeners\NotifyMetadataHasUpdated',
         ],
     ];
 
