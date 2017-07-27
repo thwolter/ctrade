94191b05e863b7f588eb3ec9ca1423eaa9d22a05
thwolter <thwolter@gmail.com>
2017-07-27 16:11:00 +0200

    clear UserSeeder from initial seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,7 +12,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(UserSeeder::class);
         $this->call(CurrencySeeder::class);
         $this->call(TransactionTypeSeeder::class);
     }
