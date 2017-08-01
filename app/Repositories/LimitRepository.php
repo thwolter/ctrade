8a2cb406018dddeb0bec8dbd70af69e1e4702d54
thwolter <thwolter@gmail.com>
2017-08-01 09:14:52 +0200

    implement time series of limit settings

--- app/Repositories/LimitRepository.php
+++ app/Repositories/LimitRepository.php
@@ -5,8 +5,11 @@ namespace App\Repositories;
 
 
 use App\Classes\Helpers;
+use App\Entities\Limit;
+use App\Entities\LimitType;
 use App\Entities\Portfolio;
 use App\Repositories\Exceptions\LimitException;
+use Carbon\Carbon;
 
 class LimitRepository
 {
@@ -20,30 +23,53 @@ class LimitRepository
     }
 
 
+    /**
+     * Deliver the time series of set limits for specified type till given date. The
+     * length of the time series is specified with parameter count but cannot start
+     * before the portfolio creation date.
+     *
+     * @param $type
+     * @param $date
+     * @param $count
+     * @return array
+     * @throws LimitException
+     */
     public function limitHistory($type, $date, $count)
     {
-        $types = ['absolute', 'relative'];
+        if (! LimitType::whereCode($type)->exists())
+            throw new LimitException("Parameter 'type' doesn't exist");
 
-        if (! in_array($type, $types)) {
-            $error = sprintf("Parameter 'type' must be one of: %s.", implode($types, ', '));
-            throw new LimitException($error);
-        }
+        $limits = $this->portfolio->limits()->whereHas('type',
+            function($query) use($type) {$query->whereCode($type); })->get();
 
-        $days = (new Helpers())->allWeekDays($date, $count);
+        $history = $this->limitsArray($limits);
 
-        $result = [];
-        foreach ($this->portfolio->limits as $limit)
-        {
-            $value = $limit->toArray();
-            $result[key($value)] = array_first($value);
-        }
+        $start = max($this->portfolio->created_at, Carbon::parse($date)->subDay($count));
+        $days = array_reverse((new Helpers())->allWeekDaysBetween($start, $date));
 
-        if (! $result[$days[0]]) $result[$days[0]] = 0;
+        if (! array_key_exists($days[0], $history)) $history[$days[0]] = 0;
         for ($i = 0; $i < count($days); $i++)
         {
-            if (! $result[$days[$i]]) {
-                $result[$days[$i]] = $result[$days[$i-1]];
-            }
+            if (! array_key_exists($days[$i], $history))
+                $history[$days[$i]] = $history[$days[$i-1]];
+        }
+
+        ksort($history);
+        return $history;
+    }
+
+    /**
+     * Create an array with date and limit value for all dates on which a limit was defined.
+     *
+     * @param  $limits
+     * @return array
+     */
+    public function limitsArray($limits)
+    {
+        $result = [];
+        foreach ($limits as $limit) {
+            $value = $limit->toArray();
+            $result[key($value)] = array_first($value);
         }
         return $result;
     }
