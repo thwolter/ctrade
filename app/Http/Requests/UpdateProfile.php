885566544100368a719324d0aec2016ae4cae9c8
thwolter <thwolter@gmail.com>
2017-08-11 16:08:14 +0200

    user can change email adress

---
+++ app/Http/Requests/UpdateProfile.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Entities\User;
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateProfile extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required|max:255',
+            'email' => 'required|email|max:255'
+        ];
+    }
+
+    /**
+     * Configure the validator instance.
+     *
+     * @param  \Illuminate\Validation\Validator  $validator
+     * @return void
+     */
+    public function withValidator($validator)
+    {
+        $validator->after(function ($validator) {
+
+            $user = User::find($this->id);
+
+            if (User::whereEmail($this->email)->where('id', '!=', $user->id)->count()) {
+                $validator->errors()->add('email', 'The email has already been taken.');
+            }
+        });
+    }
+}
