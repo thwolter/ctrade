8ee5fa3b9bd6c8b43747fe6dc924ac5327df4960
thwolter <thwolter@gmail.com>
2017-08-21 08:05:39 +0200

    bug fix metadata service binding

--- app/Listeners/Metadata/NotifyUpdatedHasCanceled.php
+++ app/Listeners/Metadata/NotifyUpdatedHasCanceled.php
@@ -33,7 +33,7 @@ class NotifyUpdatedHasCanceled implements ShouldQueue
      */
     public function handle(MetadataUpdateHasCanceled $event)
     {
-        $job = resolve("{$event->provider}/{$event->database}")
+        $job = resolve("Update{$event->provider}/{$event->database}")
             ->delay(Carbon::now()->addMinutes($this->delay));
 
         Log::info("Retry Update for {$event->provider}/{$event->database} in {$this->delay} minutes.");
--- app/Providers/MetadataServiceProvider.php
+++ app/Providers/MetadataServiceProvider.php
@@ -2,7 +2,11 @@
 
 namespace App\Providers;
 
+use App\Jobs\Metadata\UpdateQuandlECB;
+use App\Jobs\Metadata\UpdateQuandlFSE;
+use App\Jobs\Metadata\UpdateQuandlSSE;
 use App\Repositories\Metadata\Quandl\QuandlECB;
+use App\Repositories\Metadata\Quandl\QuandlFSE;
 use App\Repositories\Metadata\Quandl\QuandlSSE;
 use Illuminate\Support\ServiceProvider;
 
@@ -26,7 +30,11 @@ class MetadataServiceProvider extends ServiceProvider
     public function register()
     {
         $this->app->bind('Quandl/SSE', function($app) { return new QuandlSSE(); });
-        $this->app->bind('Quandl/FSE', function($app) { return new QuandlSSE(); });
+        $this->app->bind('Quandl/FSE', function($app) { return new QuandlFSE(); });
         $this->app->bind('Quandl/ECB', function($app) { return new QuandlECB(); });
+
+        $this->app->bind('UpdateQuandl/SSE', function($app) { return new UpdateQuandlSSE(); });
+        $this->app->bind('UpdateQuandl/FSE', function($app) { return new UpdateQuandlFSE(); });
+        $this->app->bind('UpdateQuandl/ECB', function($app) { return new UpdateQuandlECB(); });
     }
 }
