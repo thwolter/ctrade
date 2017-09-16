7af8586b7bba6eb1ad6104319de14d76b1091e6d
thwolter <thwolter@gmail.com>
2017-09-16 11:22:56 +0200

    implement PortfolioAsset Resource

--- app/Http/Resources/PortfolioAssets.php
+++ app/Http/Resources/PortfolioAssets.php
@@ -14,19 +14,34 @@ class PortfolioAssets extends Resource
      */
     public function toArray($request)
     {
-        $array = [
-            'portfolio' => [
-                'name' => $this->name,
-                'currency' => $this->currency->code,
-                'cash' => $this->cash()
-            ],
-            'assets' => []
+        return [
+            'portfolio' => $this->portfolioToArray(),
+            'assets' => $this->assetsToArray()
         ];
+    }
 
-        foreach ($this->assets()->get() as $asset)
-        {
-            $array['assets'][$asset->id] = $asset->toArray();
+    /**
+     * @param $array
+     * @return mixed
+     */
+    private function assetsToArray()
+    {
+        $array = [];
+        foreach ($this->assets()->get() as $asset) {
+            $array[$asset->id] = $asset->toArray();
         }
         return $array;
+    }
+
+    /**
+     * @return array
+     */
+    private function portfolioToArray()
+    {
+        return [
+            'name' => $this->name,
+            'currency' => $this->currency->code,
+            'cash' => $this->cash()
+        ];
     }
 }
