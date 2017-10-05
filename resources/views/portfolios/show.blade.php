946af4d5fd511e8afbf11a261e5261a778482f82
Thomas Wolter <thwolter@gmail.com>
2017-10-05 08:36:44 +0200

    small changes in dashboard

--- resources/views/portfolios/show.blade.php
+++ resources/views/portfolios/show.blade.php
@@ -2,24 +2,6 @@
 
 @section('content')
 
-    <div class="row">
-
-        <div class="col-md-3 col-sm-6">
-            @include('partials.stats.value')
-        </div> <!-- /.col-md-3 -->
-
-        <div class="col-md-3 col-sm-6">
-            @include('partials.stats.cash')
-        </div> <!-- /.col-md-3 -->
-
-        <div class="col-md-3 col-sm-6">
-            @include('partials.stats.risk')
-        </div> <!-- /.col-md-3 -->
-
-        <div class="col-md-3 col-sm-6">
-            @include('partials.stats.profit')
-        </div> <!-- /.col-md-3 -->
-    </div>
 
     <div class="row">
 
@@ -32,6 +14,25 @@
         </div>
 
         <div class="col-lg-9 col-md-8 col-sm-7">
+
+            <div class="row">
+
+                <div class="col-md-6 col-sm-6">
+                    @include('partials.stats.value')
+                </div> <!-- /.col-md-3 -->
+
+              {{--  <div class="col-md-3 col-sm-6">
+                    @include('partials.stats.cash')
+                </div>--}}
+
+                <div class="col-md-6 col-sm-6">
+                    @include('partials.stats.risk')
+                </div> <!-- /.col-md-3 -->
+
+              {{--  <div class="col-md-4 col-sm-6">
+                    @include('partials.stats.profit')
+                </div>--}}
+            </div>
 
             <portlet title="Positionen">
                 <positions-chart pid="{{ $portfolio->id }}"></positions-chart>
