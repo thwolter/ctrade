243a59951c3e96ec41fa8db045201e9e7cd81cc7
Thomas Wolter <thwolter@gmail.com>
2017-10-09 07:33:17 +0200

    adjust faq page

--- resources/views/faq/index.blade.php
+++ resources/views/faq/index.blade.php
@@ -3,6 +3,18 @@
 @section('content')
 
 
+    <!-- Breadcrumbs -->
+    <section class="g-bg-gray-light-v5">
+        <div class="container g-py-50">
+            <div class="d-sm-flex text-center">
+                <div class="align-self-center">
+                    <h2 class="h3 g-font-weight-300 w-100 g-mb-10 g-mb-0--md">FAQ</h2>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- End Breadcrumbs -->
+
     <!-- Promo Block -->
     <section class="container text-center g-pt-120 g-pb-30">
         <h2 class="g-color-black g-font-weight-600 g-font-size-50 g-mb-70">Frequently Asked Questions</h2>
@@ -42,11 +54,11 @@
                         @foreach ($category->faqs as $faq)
                             <!-- Card -->
                                 <div class="card g-brd-none rounded-0 g-mb-20">
-                                    <div id="accordion-heading-01" class="g-brd-bottom g-brd-gray-light-v4 g-pa-0"
+                                    <div id="accordion-heading-{{ $faq->id }}" class="g-brd-bottom g-brd-gray-light-v4 g-pa-0"
                                          role="tab">
                                         <h5 class="mb-0">
                                             <a class="collapsed d-flex justify-content-between g-color-main g-text-underline--none--hover rounded-0 g-px-30 g-py-20"
-                                               href="#accordion-body-01" data-toggle="collapse" data-parent="#accordion"
+                                               href="#accordion-body-{{ $faq->id }}" data-toggle="collapse" data-parent="#accordion"
                                                aria-expanded="false" aria-controls="accordion-body-01">
                                                 {{ $faq->question }}
                                                 <span class="u-accordion__control-icon">
@@ -56,8 +68,8 @@
                                             </a>
                                         </h5>
                                     </div>
-                                    <div id="accordion-body-01" class="collapse" role="tabpanel"
-                                         aria-labelledby="accordion-heading-01">
+                                    <div id="accordion-body-{{ $faq->id }}" class="collapse" role="tabpanel"
+                                         aria-labelledby="accordion-heading-{{ $faq->id }}">
                                         <div class="u-accordion__body g-color-gray-dark-v4 g-pa-30">
                                             {{ $faq->answer }}
                                         </div>
@@ -78,94 +90,6 @@
     </section>
     <!-- End Accordion -->
 
-    <div class="page-header">
-        <h3 class="page-title">@lang('faq.faq')</h3>
-
-    </div> <!-- /.page-header -->
-
-    <div class="row">
-
-        <div class="col-sm-4 col-sm-push-8">
-
-            <div class="portlet portlet-boxed">
-
-                <div class="portlet-body">
-
-                    <div class="well text-center">
-                        <p><i class="fa fa-question-circle fa-5x text-muted"></i></p>
-                        <h5>@lang('faq.hint.question')</h5>
-                        <p>@lang('faq.hint.text')</p>
-                        <a href="mailto:{{ $mail }}" class="btn btn-secondary">@lang('faq.hint.answer')</a>
-                    </div> <!-- /.well -->
-
-                    <br>
-
-                    <h5>@lang('faq.categories')</h5>
-
-                    <div class="list-group">
-
-                        @foreach($categories as $category)
-
-                            <a href="#category-{{ $category->id }}" class="list-group-item">
-                                {{ $category->name }}
-                                <i class="fa fa-chevron-right list-group-chevron"></i>
-                                <span class="badge badge-secondary">{{ $category->faqs->count() }}</span>
-                            </a>
-
-                        @endforeach
-
-
-                    </div> <!-- /.list-group -->
-                </div> <!-- /.portlet-body -->
-
-            </div> <!-- /.portlet -->
-
-        </div> <!-- /.col -->
-
-
-        <div id="faq-questions" class="col-sm-8 col-sm-pull-4">
-
-            <div class="portlet portlet-boxed">
-                <div class="portlet-body">
-
-                    @foreach ($categories as $category)
-                        <h5 id="category-{{ $category->id }}">{{ $category->name }}</h5>
-
-                        <div id="accordion-{{ $category->id }}" class="panel-group accordion-simple">
-
-                            @foreach ($category->faqs as $faq)
-                                <div class="panel">
-                                    <div class="panel-heading">
-                                        <h4 class="panel-title">
-                                            <a class="accordion-toggle" data-toggle="collapse"
-                                               data-parent="#accordion-{{ $category->id }}"
-                                               href="#faq-general-{{ $faq->id }}">
-                                                <i class="fa faq-accordion-caret"></i>{{ $faq->question }}
-                                            </a>
-                                        </h4>
-                                    </div><!-- .panel-heading -->
-
-                                    <div id="faq-general-{{ $faq->id }}" class="panel-collapse collapse">
-                                        <div class="panel-body">
-                                            <p>{{ $faq->answer }}</p>
-                                        </div><!-- .panel-body -->
-                                    </div> <!-- ./panel-collapse -->
-                                </div><!-- .panel -->
-                            @endforeach
-
-                        </div> <!-- /.accordion -->
-
-                        <br class="xs-20">
-                    @endforeach
-
-
-                </div> <!-- /.portlet-body -->
-
-            </div> <!-- /.portlet -->
-
-        </div> <!-- /.col -->
-
-    </div> <!-- /.row -->
 
 @endsection
 
@@ -177,4 +101,21 @@
 
 @section('script.footer')
 
+    <!-- JS Unify -->
+    <script src="{{ asset('assets/js/components/hs.tabs.js') }}"></script>
+
+    <!-- JS Plugins Init. -->
+    <script>
+        $(document).on('ready', function () {
+
+            // initialization of tabs
+            $.HSCore.components.HSTabs.init('[role="tablist"]');
+        });
+
+        $(window).on('resize', function () {
+            setTimeout(function () {
+                $.HSCore.components.HSTabs.init('[role="tablist"]');
+            }, 200);
+        });
+    </script>
 @endsection
