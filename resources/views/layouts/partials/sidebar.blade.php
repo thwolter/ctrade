49593cdc887ef3184f0b50a61266ce1136a81fc7
Thomas Wolter <thwolter@gmail.com>
2017-10-12 17:51:48 +0200

    sidebar as a menu

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -6,39 +6,37 @@
                href="#">Neue Transaktion</a>
         </div>
 
-        <!-- Links -->
-        <div class="g-mb-50">
-            <h3 class="h5 g-color-black g-font-weight-600 mb-4">Links</h3>
-            <ul class="list-unstyled g-font-size-13 mb-0">
-                <li>
-                    <a class="d-block u-link-v5 g-color-gray-dark-v4 rounded g-px-20 g-py-8 {{ active_class(if_route('portfolios.show')) }}"
-                       href="{{ route('portfolios.show', $portfolio->slug) }}">
-                        <i class="mr-2 fa fa-angle-right"></i>@lang('navigation.dashboard')
-                    </a>
-                </li>
-                <li>
-                    <a class="d-block u-link-v5 g-color-gray-dark-v4 rounded g-px-20 g-py-8 {{ active_class(if_route_pattern(['transactions.*'])) }}"
-                       href="{{ route('transactions.index', $portfolio->slug) }}">
-                        <i class="mr-2 fa fa-angle-right"></i>@lang('navigation.transactions')
-                    </a>
-                </li>
-                <li>
-                    <a class="d-block u-link-v5 g-color-gray-dark-v4 rounded g-px-20 g-py-8 {{ active_class(if_route_pattern(['positions.*', 'assets.*'])) }}"
-                       href="{{ route('positions.index', $portfolio->slug) }}">
-                        <i class="mr-2 fa fa-angle-right"></i>@lang('navigation.positions')
-                    </a>
-                </li>
-                <li>
-                    <a class="d-block u-link-v5 g-color-gray-dark-v4 rounded g-px-20 g-py-8"
-                       href="{{ route('home.coming') }}">
-                        <i class="mr-2 fa fa-angle-right"></i>@lang('navigation.optimize')</a>
-                </li>
-                <li>
-                    <a class="d-block u-link-v5 g-color-gray-dark-v4 rounded g-px-20 g-py-8 {{ active_class(if_route('portfolios.edit')) }}"
-                       href="{{ route('portfolios.edit', ['slug' => $portfolio->slug]) }}">
-                        <i class="mr-2 fa fa-angle-right"></i>@lang('navigation.settings.title')</a>
-                </li>
-            </ul>
+    @php
+        $activeClass = 'g-bg-bluegray-lineargradient g-brd-none g-color-gray-light-v4';
+    @endphp
+
+    <!-- Links -->
+        <div class="list-group g-mb-50">
+            <h3 class="h5 g-color-black g-font-weight-600 mb-4">Portfolio</h3>
+
+            <a class="list-group-item {{ if_route('portfolios.show') ? $activeClass : 'g-color-gray-dark-v2' }}"
+               href="{{ route('portfolios.show', $portfolio->slug) }}">
+                <i class="mr-2 fa fa-angle-right"></i>@lang('navigation.dashboard')
+            </a>
+
+            <a class="list-group-item {{ if_route_pattern(['transactions.*']) ? $activeClass : 'g-color-gray-dark-v2' }}"
+               href="{{ route('transactions.index', $portfolio->slug) }}">
+                <i class="mr-2 fa fa-angle-right"></i>@lang('navigation.transactions')
+            </a>
+
+            <a class="list-group-item {{ if_route_pattern(['positions.*', 'assets.*']) ? $activeClass : 'g-color-gray-dark-v2' }}"
+               href="{{ route('positions.index', $portfolio->slug) }}">
+                <i class="mr-2 fa fa-angle-right"></i>@lang('navigation.positions')
+            </a>
+
+            <a class="list-group-item g-color-gray-dark-v2"
+               href="{{ route('home.coming') }}">
+                <i class="mr-2 fa fa-angle-right"></i>@lang('navigation.optimize')</a>
+
+            <a class="list-group-item {{ if_route('portfolios.edit') ? $activeClass : 'g-color-gray-dark-v2' }}"
+               href="{{ route('portfolios.edit', ['slug' => $portfolio->slug]) }}">
+                <i class="mr-2 fa fa-angle-right"></i>@lang('navigation.settings.title')</a>
         </div>
     </div>
 </div>
+
--- resources/views/positions/index.blade.php
+++ resources/views/positions/index.blade.php
@@ -2,22 +2,39 @@
 
 @section('content')
 
-    <div class="row">
-        <div class="col-md-6">
-            @include('positions.cash.index')
+    @include('layouts.partials.header')
+
+    <div class="container g-pt-100 g-pb-20">
+        <div class="row justify-content-between">
+
+            <!-- Sidebar -->
+            @include('layouts.partials.sidebar')
+
+            <!-- Main section -->
+            <div class="col-lg-9 order-lg-2 g-mb-80">
+
+                @unless ( $portfolio->payments->count() )
+                    <div class="alert alert-info alert-dismissible role=" role="alert">
+                        <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span
+                                    aria-hidden="true">&times;</span></button>
+                        <strong>Noch keine Positionen vorhanden.</strong>
+                    </div>
+                @endunless
+
+                <div class="u-heading-v3-1 g-mb-40">
+                    <h2 class="h3 u-heading-v3__title">Positionen</h2>
+                </div>
+
+                @include('positions.stocks.index')
+
+            </div>
         </div>
-
-        <div class="col-md-3 col-sm-6">
-            @include('partials.stats.value')
-        </div> <!-- /.col-md-3 -->
-
-        <div class="col-md-3 col-sm-6">
-            @include('partials.stats.profit')
-        </div> <!-- /.col-md-3 -->
-
     </div>
 
-    @include('positions.stocks.index')
+@endsection
+
+
+@section('script.footer')
 
 @endsection
 
