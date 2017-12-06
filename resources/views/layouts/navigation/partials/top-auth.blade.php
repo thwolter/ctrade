eec8f920ae2c6008908f2bfd9772c0bfbdc6ccf2
Thomas Wolter <thwolter@gmail.com>
2017-12-06 10:46:58 +0100

    login as button

---
+++ resources/views/layouts/navigation/partials/top-auth.blade.php
@@ -0,0 +1,94 @@
+@guest
+    <li class="nav-item g-my-8 g-mx-5">
+        <a href="{{ route('login') }}"
+           class="nav-link g-bg-gray-light-v2 g-color-gray-dark-v2 g-bg-gray-light-v4--hover g-font-weight-600 g-font-size-default g-px-17">
+            Login
+        </a>
+    </li>
+
+    <li class="nav-item g-my-8 g-mx-5">
+        <a href="{{ route('register') }}"
+           class="nav-link g-bg-primary g-bg-primary-opacity-0_8--hover g-font-weight-600 g-font-size-default g-px-17">
+            Register
+        </a>
+    </li>
+@endguest
+
+@auth
+    <li class="nav-item g-my-8 g-pos-rel">
+        <a id="account-dropdown-invoker"
+           class="nav-link g-bg-primary-dark-v1--hover g-font-weight-600 g-font-size-default g-px-17 g-px-23--xl"
+           href="#"
+           aria-controls="account-dropdown"
+           aria-haspopup="true"
+           aria-expanded="false"
+           data-dropdown-event="hover"
+           data-dropdown-target="#account-dropdown"
+           data-dropdown-type="css-animation"
+           data-dropdown-duration="0"
+           data-dropdown-hide-on-scroll="true"
+           data-dropdown-animation-in="fadeIn"
+           data-dropdown-animation-out="fadeOut">
+            Account
+            <i class="ml-2 fa fa-angle-down"></i>
+        </a>
+
+        <ul id="account-dropdown"
+            class="list-unstyled u-shadow-v11 g-min-width-220 g-bg-white g-pos-abs g-left-0 g-z-index-99 g-mt-28"
+            aria-labelledby="account-dropdown-invoker">
+
+            <li class="dropdown-item g-px-5">
+                <a class="nav-link g-color-text g-font-weight-600"
+                   href="{{ route('users.edit', ['tab' => 'profile']) }}">
+                    Mein Profil
+                </a>
+            </li>
+
+            <li class="dropdown-item g-px-5">
+                <a class="nav-link g-color-text g-font-weight-600"
+                   href="{{ route('users.edit', ['tab' => 'password']) }}">
+                    Passwort
+                </a>
+            </li>
+
+            <li class="dropdown-item g-px-5">
+                <a class="nav-link g-color-text g-font-weight-600"
+                   href="{{ route('users.edit', ['tab' => 'messaging']) }}">
+                    Emails
+                </a>
+            </li>
+
+            <li class="dropdown-item g-px-5">
+                <a class="nav-link g-color-text g-font-weight-600"
+                   href="{{ route('logout') }}"
+                   onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                    Logout
+                </a>
+            </li>
+
+            @role('admin')
+
+            <li class="dropdown-item g-px-5">
+                <a href="/admin/dashboard"
+                   target="_blank"
+                   class="nav-link g-color-text g-font-weight-600">
+                    Backpack
+                </a>
+            </li>
+            <li class="dropdown-item g-px-5">
+                <a href="/horizon"
+                   target="_blank"
+                   class="nav-link g-color-text g-font-weight-600">
+                    Horizon
+                </a>
+            </li>
+
+            @endrole
+
+            <form id="logout-form" action="{{ route('logout') }}" method="POST"
+                  style="display: none;">
+                {{ csrf_field() }}
+            </form>
+        </ul>
+    </li>
+@endauth
--- resources/views/layouts/navigation/partials/top-nav.blade.php
+++ resources/views/layouts/navigation/partials/top-nav.blade.php
@@ -3,35 +3,35 @@
      class="collapse navbar-collapse align-items-center justify-content-end order-lg-last g-brd-top g-brd-none--lg g-brd-primary-dark-v1">
     <ul class="navbar-nav g-py-30 g-py-0--lg">
 
-        <li class="nav-item">
+        <li class="nav-item g-my-8">
             <a href="#"
-               class="nav-link g-bg-primary-dark-v1--hover g-font-weight-600 g-font-size-default g-px-17 g-px-23--xl g-py-15">
+               class="nav-link g-bg-primary-dark-v1--hover g-font-weight-600 g-font-size-default g-px-17 g-px-23--xl">
                 Home
             </a>
         </li>
 
-        <li class="nav-item {{ active_class(if_route_pattern(['blog'])) }}">
+        <li class="nav-item g-my-8 {{ active_class(if_route_pattern(['blog'])) }}">
             <a href="{{ route('home.blog') }}"
-               class="nav-link g-bg-primary-dark-v1--hover g-font-weight-600 g-font-size-default g-px-17 g-px-23--xl g-py-15">
+               class="nav-link g-bg-primary-dark-v1--hover g-font-weight-600 g-font-size-default g-px-17 g-px-23--xl">
                 Blog
             </a>
         </li>
 
-        <li class="nav-item {{ active_class(if_route('home.contact')) }}">
+        <li class="nav-item g-my-8 {{ active_class(if_route('home.contact')) }}">
             <a href="{{ route('home.contact') }}"
-               class="nav-link g-bg-primary-dark-v1--hover g-font-weight-600 g-font-size-default g-px-17 g-px-23--xl g-py-15">
+               class="nav-link g-bg-primary-dark-v1--hover g-font-weight-600 g-font-size-default g-px-17 g-px-23--xl">
                 Kontakt
             </a>
         </li>
 
-        <li class="nav-item {{ active_class(if_route('home.about')) }}">
+        <li class="nav-item g-my-8 {{ active_class(if_route('home.about')) }}">
             <a href="{{ route('home.about') }}"
-               class="nav-link g-bg-primary-dark-v1--hover g-font-weight-600 g-font-size-default g-px-17 g-px-23--xl g-py-15">
+               class="nav-link g-bg-primary-dark-v1--hover g-font-weight-600 g-font-size-default g-px-17 g-px-23--xl">
                 Über uns
             </a>
         </li>
 
-       @include('top-auth')
+       @include('layouts.navigation.partials.top-auth')
 
     </ul>
 </div>
