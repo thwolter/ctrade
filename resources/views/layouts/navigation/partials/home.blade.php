93c6d46ffe56eed01362f8d2c168345b67aba12b
Thomas Wolter <thwolter@gmail.com>
2017-12-13 07:02:00 +0100

    make btn for authenticated home

--- resources/views/layouts/navigation/partials/home.blade.php
+++ resources/views/layouts/navigation/partials/home.blade.php
@@ -1,7 +1,7 @@
 @auth
     <li class="nav-item g-my-8">
         <a href="{{ route('portfolios.index') }}"
-           class="nav-link g-bg-primary-dark-v1--hover g-font-weight-600 g-font-size-default g-px-17 g-px-23--xl">
+           class="nav-link btn u-btn-outline-primary g-font-weight-600 g-font-size-default g-px-17 g-px-23--xl">
             Portfolios
         </a>
     </li>
