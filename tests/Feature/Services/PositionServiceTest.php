8391310379b0e0fb392517cc47cc63d45972b227
Thomas Wolter <thwolter@gmail.com>
2018-02-11 17:52:36 +0100

    empty PositionServiceTest

--- tests/Feature/Services/PositionServiceTest.php
+++ tests/Feature/Services/PositionServiceTest.php
@@ -2,6 +2,9 @@
 
 namespace Tests\Feature\Services;
 
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class PositionServiceTest extends TestCase
 {
